0829136c0e3299f5f0d726dd730e5608fc8e6c2d
bvpav <bvpav@tutanota.com>
2022-05-21 19:07:54 +0300

    test: :white_check_mark: add lowercase alpha tests
    ensures that getUniqueChars functions only return lowercase alphabet characters

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -9,10 +9,34 @@ describe(get3UniqueChars, () => {
     const chars = get3UniqueChars();
     expect(chars[0] === chars[1] && chars[1] === chars[2]).toBe(false);
   });
+
+  it('only returns lowercase alphabet characters', () => {
+    expect(
+      get3UniqueChars()
+        .split('')
+        .find(
+          (c) =>
+            c.charCodeAt(0) < 'a'.charCodeAt(0) ||
+            c.charCodeAt(0) > 'z'.charCodeAt(0)
+        )
+    ).toBeUndefined();
+  });
 });
 
 describe(get3Chars, () => {
   it('generates exactly 3 chars', () => {
     expect(get3Chars().length).toBe(3);
+  });
+
+  it('only returns lowercase alphabet characters', () => {
+    expect(
+      get3UniqueChars()
+        .split('')
+        .find(
+          (c) =>
+            c.charCodeAt(0) < 'a'.charCodeAt(0) ||
+            c.charCodeAt(0) > 'z'.charCodeAt(0)
+        )
+    ).toBeUndefined();
   });
 });
